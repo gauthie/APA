e254ce327ee4bf32db4c4bd6ab7a9ffbfe8ce510
me <roussel.gauthier@gmail.com>
2021-05-09 17:39:17 +0200

    view

---
+++ app/src/main/java/com/example/appdoc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.appdoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.consult);
+    }
+}
